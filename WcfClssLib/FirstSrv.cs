60d74f0d40cd2fff5fe42484b526a5b6fc309434
Javier Mendoza <javier.mendoza@avantica.com>
2020-12-28 09:07:52 -0500

    Add wcf service

---
+++ WcfClssLib/FirstSrv.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfClssLib
+{
+    public class FirstSrv : IFirstSrv
+    {
+        public string GetMessage(string txt)
+        {
+            return "This is the First WCF Service.";
+        }
+    }
+}
---
+++ WcfClssLib/IFirstSrv.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace WcfClssLib
+{
+    [ServiceContract]
+    public interface IFirstSrv
+    {
+        [OperationContract]
+        string GetMessage(string txt);
+    }
+}
